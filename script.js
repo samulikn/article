bc7a087265cc4c53be2f8bf7b0ed39046f3ae454
Nataliia Shobotova <samulikn@gmail.com>
2025-09-19 11:30:56 +0200

    added script on share button

---
+++ script.js
@@ -0,0 +1,35 @@
+const shareBtn = document.getElementById("sharebtn");
+const shareIcon = document.getElementById("shareicon");
+const shareTo = document.getElementById("shareto");
+const triangle = document.getElementById("triangle");
+
+// Show social media links
+shareBtn.onclick = () => {
+  shareTo.style.visibility = "visible";
+  triangle.style.visibility = "visible";
+
+  shareBtn.classList.add("sharebtn-active");
+  shareIcon.classList.add("shareicon-active");
+};
+
+const removeActiveState = () => {
+  shareTo.style.visibility = "hidden";
+  triangle.style.visibility = "hidden";
+
+  shareBtn.classList.remove("sharebtn-active");
+  shareIcon.classList.remove("shareicon-active");
+};
+
+// Close sharing container on Esc button
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") {
+    removeActiveState();
+  }
+});
+
+// When the user clicks anywhere outside of the sharing content, close it
+document.addEventListener("click", (e) => {
+  if (!shareBtn.contains(e.target) && !shareTo.contains(e.target)) {
+    removeActiveState();
+  }
+});
